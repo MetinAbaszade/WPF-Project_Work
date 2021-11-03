7e8f9206e160482a9563e5de20d0044eeb58b11e
MetinAbaszade <metin.abaszade@gmail.com>
2021-11-03 15:56:00 +0400

    First Empty Project

---
+++ Project_Work_WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Maps.MapControl.WPF;
+using BingMapsRESTToolkit;
+
+namespace Project_Work_WPF.ViewModels
+{
+	class MainViewModel
+	{
+	}
+}
